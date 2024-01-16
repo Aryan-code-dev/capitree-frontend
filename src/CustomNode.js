49cf7eb64fb458e8bcf8d32bf47c1e05646e9514
Aryan Dande <86364775+Aryan-code-dev@users.noreply.github.com>
2024-01-16 11:20:28 +0530

    initial commit

---
+++ src/CustomNode.js
@@ -0,0 +1,15 @@
+import { useCallback } from 'react';
+import { Handle, Position } from 'reactflow';
+
+function CustomNode({ data, isConnectable }) {
+    return (
+      <div style={{ border: '1px solid #eee', padding: '10px', borderRadius: '4px', background: '#eee' }}>
+        <div>Path: '{data.label}'</div>
+        <div>Return Type: {data.returnType}</div>
+        <Handle type="source" position={Position.Right}  isConnectable={isConnectable} />
+        <Handle type="target" position={Position.Left}  isConnectable={isConnectable} />
+      </div>
+    );
+  }
+
+  export default CustomNode;
